a4a3cc5416422b5dcb64a08d37cda318bd381444
unknown <senanaal@code.edu.az>
2024-04-30 23:33:45 +0400

    FinalBACK

---
+++ WebApplication6/Areas/Admin/ViewModel/UserUpdateViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication6.Areas.Admin.ViewModel
+{
+    public class UserUpdateViewModel
+    {
+        public IList<string> Roles { get; set; } = null!;
+    }
+}
---
+++ WebApplication6/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication6.Context;
+using WebApplication6.Models;
+using WebApplication6.ViewModel;
+
+namespace WebApplication6.ViewComponents;
+
+public class HeaderViewComponent : ViewComponent
+{
+	private readonly RioDbContext _context;
+	private readonly UserManager<AppUser> _userManager;
+
+	public HeaderViewComponent(RioDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		var user = await _userManager.FindByNameAsync(User.Identity.Name);
+		var settings = await _context.Settings.ToDictionaryAsync(x => x.Key, x => x.Value);
+		var basketItems = await _context.BasketItems.Where(b => b.AppUserId == user.Id).ToListAsync();
+
+		HeaderViewModel headerViewModel = new HeaderViewModel
+		{
+			Settings = settings,
+			BasketItems = basketItems,
+
+		};
+
+		return View(settings);
+	}
+}
